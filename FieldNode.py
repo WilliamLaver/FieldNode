826d4e14a8d6347829f1987f929befb754f2d3e0
WilliamLaver <gwilliam.laver@gmail.com>
2023-12-20 16:30:33 -0500

    fleshed out Node object, need the inter-nodes to have weights added and formulate a simple Impulse to test

--- FieldNode.py
+++ FieldNode.py
@@ -18,25 +18,46 @@ class Field(object):
     def __init__(self):
 
         self.name = "Field"
-        self.field = pd.DataFrame()
+        self.field = None
         iVec = np.array([1, 0, 1, 1, 1])
-        origin = Node(1, iVec, 0)
-        Impulse(0, origin, cl=1)
+
+        self.init_nodes()
+        #Impulse(0, origin, cl=1)
+
+    def init_nodes(self):
+        n = 50
+        self.field = np.array([[None for i in range(n)] for j in range(n)])
+        for i in range(n):
+            for j in range(n):
+                self.field[i][j] = Node(np.array([i, j]))
 
     def display_node_vals(self):
-        # For each node in the field, display the value. Result should be 
+        # For each node in the field, display the value. Result should be
         # visually rendered at the very least in 2D.
-        pass
+        out = [[0 for i in range(len(self.field))]
+               for j in range(len(self.field[0]))]
+        for i in range(len(self.field)):
+            for j in range(len(self.field[0])):
+                out[i][j] = self.field[i][j].g
+        print(out)
 
 
 class Node(object):
 
-    def __init__(self):
+    def __init__(self, coordinates):
         # neighbours is the collection of weights assigned to surrounding
         # Nodes, these weights should increment each time the path is taken:
         # effectively greasing the most active pathways
-        self.neigbours = np.array([None, None, None, None])
+        self.pos = coordinates
+        self.neigbours = self.set_neighbours(self.pos)
         self.g = 0
+
+    # each neighbour should have a weight ("grease") value associated with it
+    def set_neighbours(self, pos):
+        self.neighbours = [pos + np.array([1, 0]),
+                           pos + np.array([-1, 0]),
+                           pos + np.array([0, 1]),
+                           pos + np.array([0, -1])]
 
     # inacted when an impulse "crosses" the node
     def pulse(self):
@@ -50,12 +71,15 @@ class Node(object):
         # four value array (N/E/S/W in 2D)
         pass
 
+
+# Each impulse should propagate from an origin Node to each subsquent
+# neighbour Node in accordance with the weights
 class Impulse(object):
 
     # G is the new value at the vector as a function of the old
     # nVec is the vector with which the future node will be created
     # cl is the CHAIN LENGTH of connected nodes so far
-    def __init__(self, g, pVec, cl):
+    def __init__(self, field, g, pVec, cl):
  
         self.cl = cl + 1
         self.g = self.f(g)
@@ -71,7 +95,7 @@ class Impulse(object):
     # increment the g factor (grease factor)
     # decreases signal according to the length of the chain
     def f(self, g):
-        return g + (1 - self.cl / 100)
+        return g + (1 - self.cl / 25)
 
     def vecfunc(self, r):
         return r
